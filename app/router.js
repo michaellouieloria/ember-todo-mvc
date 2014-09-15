8d121fe672fc04b62d7bedc4e2e71c662bac20f2
Michael Louie Loria <michaellouieloria@gmail.com>
2014-09-15 15:31:18 +0800

    initial commit

---
+++ app/router.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: TodoMvcENV.locationType
+});
+
+Router.map(function() {
+  this.resource('todos', { path: '/' }, function() {
+      this.route('active');
+      this.route('complete');
+  });
+  this.route('todos/active');
+  this.route('todos/complete');
+});
+
+export default Router;
